f82c56ef4cd56b564cbb27f2bc8d88c8d37f7328
Piyush Mishra <piyush_mishra31@outlook.com>
2021-04-21 10:29:48 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import TextField from "@material-ui/core/TextField";
+import { useState } from "react";
+import readingTime from "reading-time";
+import { withStyles } from "@material-ui/core/styles";
 
-function App() {
+const styles = {
+  root: {
+    background: "black",
+  },
+  input: {
+    color: "white",
+  },
+};
+
+const App = (props) => {
+  const { classes } = props;
+  let [readtime, setReadTime] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <TextField
+          style={{ width: "85%" }}
           // id="outlined-multiline-static"
           // label="Multiline"
           multiline
-          // rows={4}
+          onChange={(e) => {
+            e.persist();
+            const stats = readingTime(e.target.value);
+            setReadTime(stats.text);
+          }}
+          InputProps={{
+            className: classes.input,
+          }}
+          rows={25}
+          color={"secondary"}
           // defaultValue="Default Value"
           // variant="outlined"
         />
+        <div>{readtime}</div>
       </header>
     </div>
   );
-}
+};
 
-export default App;
+export default withStyles(styles)(App);
